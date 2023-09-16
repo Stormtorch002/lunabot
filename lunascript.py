194081c74813f83c7b36074b1a7dbd987fc19e12
Stormtorch002 <pianister123@gmail.com>
2023-09-16 16:56:29 -0400

    ez

--- lunascript.py
+++ lunascript.py
@@ -4,6 +4,27 @@ import expr
 from num2words import num2words
 
 
+def lunascript_var(aliases=None):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            self.vars[func.__name__] = func
+            if aliases is not None:
+                for alias in aliases:
+                    self.vars[alias] = func
+            return func(self, *args, **kwargs)
+        return wrapper   
+    return decorator
+
+def lunascript_func(aliases=None):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            self.funcs[func.__name__] = func
+            if aliases is not None:
+                for alias in aliases:
+                    self.funcs[alias] = func
+            return func(self, *args, **kwargs)
+        return wrapper   
+    return decorator
 
 class ScriptContext:
     def __init__(self, bot, guild, channel, member=None, message=None):
@@ -13,27 +34,27 @@ class ScriptContext:
         self.member = member 
         self.message = message 
 
-        var_methods = [
-            self.server,
-            self.members,
-            self.boosts,
-            self.boostlevel,
-            self.channel,
-            self.channelname,
-            self.member,
-            self.avatar,
-            self.memberusername,
-            self.membername
-        ]
-        func_methods = [
-            self.th
-        ]
-        self.vars = {} 
-        self.funcs = {}
-        for method in var_methods:
-            method() 
-        for method in func_methods:
-            method()
+        # var_methods = [
+        #     self.server,
+        #     self.members,
+        #     self.boosts,
+        #     self.boostlevel,
+        #     self.channel,
+        #     self.channelname,
+        #     self.member,
+        #     self.avatar,
+        #     self.memberusername,
+        #     self.membername
+        # ]
+        # func_methods = [
+        #     self.th
+        # ]
+        # self.vars = {} 
+        # self.funcs = {}
+        # for method in var_methods:
+        #     method() 
+        # for method in func_methods:
+        #     method()
 
     @classmethod 
     def from_ctx(cls, ctx):
@@ -41,29 +62,6 @@ class ScriptContext:
     
     # make a decorator that will add the function to self.repls
     # make it take an optional argument called aliases 
-
-    
-    def lunascript_var(aliases=None):
-        def decorator(func):
-            def wrapper(self, *args, **kwargs):
-                self.vars[func.__name__] = func
-                if aliases is not None:
-                    for alias in aliases:
-                        self.vars[alias] = func
-                return func(self, *args, **kwargs)
-            return wrapper   
-        return decorator
-    
-    def lunascript_func(aliases=None):
-        def decorator(func):
-            def wrapper(self, *args, **kwargs):
-                self.funcs[func.__name__] = func
-                if aliases is not None:
-                    for alias in aliases:
-                        self.funcs[alias] = func
-                return func(self, *args, **kwargs)
-            return wrapper   
-        return decorator
 
     @lunascript_var(aliases=['servername'])
     def server(self):
